212d21fb4a8053570d2dd42fd76b6dc33a985081
Armandsv11 <112925805+Armandsv11@users.noreply.github.com>
2022-10-18 14:03:07 +0300

    Add files via upload

---
+++ operations_test.c
@@ -0,0 +1,32 @@
+// operācijas				operands_1 operācija_a > operands_2
+//							||
+//							\/
+//						operācijas rezultāts =
+// matemātiskas					(datu tips) = nereti lielākais datu tips programmā
+// loģiskās
+// loģiskās pa bitiem
+// bitu opp.
+// ko pētīt šodien: kā strādā operācija?
+//		    vai operācijā var izmantot jebkuru datu tipu?
+//		    operāciju izplatīšanas secība? (to nosaka prioritāte
+//						    priorītātes kontrolei izmanto () )
+
+#include<stdio.h>
+
+int(main)
+{
+char C = 'A';
+int i = 2000;
+float f = 2.3;
+double d = -5.6e4;
+
+printf("%d (%d bytes) * \d (\d bytes)", c1,sizeof(c1),i1,sizeof(i1));
+printf(" = %d (%d bytes)\n",c1*i1,sizeof(c1*i1));
+
+// >
+printf("\n%, 1f (%ld bytes) > %.2f (%ld bytes)",f1,sizeof(f1),f2,sizeof(f2));
+//printf(" = %.1f (%ld bytes) \n\m", f1>f2,sizeof(f1>f2));
+printf(" = %d (%ld bytes)\n\n",f1>f2,sizeof(f1>f2));
+
+return 0;
+}
